6dcbae7d10d631be50e18ed88fb350d6e7fc7348
nanaakuffo1 <kwabena.akuffo@gmail.com>
2026-05-15 17:20:43 -0500

    Configure Supabase auth

--- public/supabase-config.js
+++ public/supabase-config.js
@@ -1,4 +1,4 @@
 window.OPTIONAI_SUPABASE = {
-  url: "https://YOUR_PROJECT_REF.supabase.co",
-  anonKey: "YOUR_SUPABASE_ANON_KEY"
+  url: "https://zdhdyrmmbcysgketpfsx.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpkaGR5cm1tYmN5c2drZXRwZnN4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4NjgzMjYsImV4cCI6MjA5NDQ0NDMyNn0.ME3lkIkQSZcfAEEsEzdvFkzZMINu-KNoZzmZlWzkv7k"
 };
